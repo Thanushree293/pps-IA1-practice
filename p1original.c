9ade6f1eb9becaadd6ea488b7ef8eefd086250f1
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-02-02 04:35:28 +0000

    add of two numbers

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter the two numbers\n");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("addition of two numbers %d+%d is %d\n",a,b,sum);
+}
+int main()
+{
+  int a,b,c,ans;
+   input(&a,&b);
+   add(a,b,&ans);
+  output(a,b,ans);
+  return 0;
 }
